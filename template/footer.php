e613ab19d4de723079edee71a29ddcb131057221
Mattias Geniar <mattias.geniar@gmail.com>
2010-09-19 20:24:56 +0200

    Updated tracker PNG

--- template/footer.php
+++ template/footer.php
@@ -1,3 +1,7 @@
+
+<!-- Some basic usage tracking. Nice stats on browser agents etc. 
+So I know on which browser(s) to focus most -->
+<img src="http://www.mozbx.net/images/tracker.png" />
 </body>
 </html>
 
