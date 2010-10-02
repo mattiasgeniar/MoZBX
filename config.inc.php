98866a9351749c0ed567efe7b7d4a11683b3039a
Mattias Geniar <mattias.geniar@gmail.com>
2010-10-02 15:45:55 +0200

    Updated general config

--- config.inc.php
+++ config.inc.php
@@ -1,10 +1,10 @@
 <?php
-if (strpos($_SERVER['SERVER_NAME'], 'demo') !== false)
-        require_once("config-demo.php");
-else {
-    if (file_exists("config.php"))
-        require_once("config.php");
-    else
-        require_once("config-live.php");
-}
+	if (strpos($_SERVER['SERVER_NAME'], 'demo') !== false)
+		require_once("config-demo.php");
+	else {
+		if (file_exists("config.php"))
+			require_once("config.php");
+		else
+			require_once("config-live.php");
+	}
 ?>
