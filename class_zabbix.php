55285c52c93a569a22793832e9e5a66d2fa9df66
Mattias Geniar <mattias.geniar@gmail.com>
2011-03-04 13:36:44 +0100

    Curl_opt parameter to allow self-signed certificates in Zabbix.

--- class_zabbix.php
+++ class_zabbix.php
@@ -428,6 +428,7 @@
 			curl_setopt($ch, CURLOPT_URL,  $this->zabbix_url_index);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 			$post_data = array(
 				'name' => $this->getUsername(),
 				'password' => $this->getPassword(),
