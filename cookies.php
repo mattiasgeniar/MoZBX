0e87839007441bc2de93c7146c9c10da028be089
Mattias Geniar <mattias.geniar@gmail.com>
2011-07-25 00:09:34 +0200

    Login: give preference to POST data, then COOKIE.

--- cookies.php
+++ cookies.php
@@ -1,26 +1,26 @@
 <?php
 
 	// Zabbix JSON API url
-	if (isset($_COOKIE['zabbixApi']))
-		$zabbixApi = $_COOKIE['zabbixApi'];
-	elseif (isset($_POST['zabbixApi']))
+	if (isset($_POST['zabbixApi']))
 		$zabbixApi = $_POST['zabbixApi'];
+	elseif (isset($_COOKIE['zabbixApi']))
+		$zabbixApi = $_COOKIE['zabbixApi'];
 	else
 		$zabbixApi = $arrSettings["zabbixApiUrl"];
 	
 	// Zabbix Username
-	if (isset($_COOKIE['zabbixUsername']))
-		$zabbixUser = $_COOKIE['zabbixUsername'];
-	elseif (isset($_POST['zabbixUsername']))
+	if (isset($_POST['zabbixUsername']))
 		$zabbixUser = $_POST['zabbixUsername'];
+	elseif (isset($_COOKIE['zabbixUsername']))
+		$zabbixUser = $_COOKIE['zabbixUsername'];
 	else
 		$zabbixUser = $arrSettings["zabbixUsername"];
 		
 	// Zabbix Password
-	if (isset($_COOKIE['zabbixPassword']))
-		$zabbixPass = $_COOKIE['zabbixPassword'];
-	elseif (isset($_POST['zabbixPassword']))
+	if (isset($_POST['zabbixPassword']))
 		$zabbixPass = $_POST['zabbixPassword'];
+	elseif (isset($_COOKIE['zabbixPassword']))
+		$zabbixPass = $_COOKIE['zabbixPassword'];
 	else
 		$zabbixPass = $arrSettings["zabbixPassword"];
 		
