9aed67a4f03014adab83812230b08df8e792cc9d
Mattias Geniar <mattias.geniar@gmail.com>
2011-07-25 00:10:17 +0200

    Changed logout to actually work now.

--- logout.php
+++ logout.php
@@ -1,9 +1,7 @@
 <?php
 	// Destroy cookies
-	//setcookie("zabbixUsername", "", time() - 1);
-	//setcookie("zabbixApi", "", time() - 1);
-	setcookie("zabbixPassword", "", time() - 1);
-	setcookie("zabbixAuthHash", "", time() - 1);	
+	setcookie("zabbixPassword", "bogus", time() + 1);
+	setcookie("zabbixAuthHash", "", time() - 100);
 	
 	header("Location: index.php");
 	exit();
