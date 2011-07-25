9eb487fad9b664441d6727862920966b2737aa9b
Mattias Geniar <mattias.geniar@gmail.com>
2011-07-25 18:26:08 +0200

    Modified the config loading to allow overwrites

--- config.inc.php
+++ config.inc.php
@@ -2,9 +2,12 @@
 	if (strpos($_SERVER['SERVER_NAME'], 'demo') !== false)
 		require_once("config-demo.php");
 	else {
-		if (file_exists("config.php"))
+        // Load the default configuration file
+        if (file_exists("config-live.php"))
+            require_once("config-live.php");
+        
+        // If a custom config.php file exists, load it to overwrite the above.
+        if (file_exists("config.php"))
 			require_once("config.php");
-		else
-			require_once("config-live.php");
 	}
 ?>
