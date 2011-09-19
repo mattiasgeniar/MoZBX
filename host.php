b978c2bcd703f7bcf8afeab25b2f6e4f9c61f60e
H. Westphal <westphal@secure-net-concepts.de>
2011-09-19 21:28:08 +0200

    Replaced short open tags.

--- host.php
+++ host.php
@@ -3,35 +3,35 @@
 	require_once("functions.php");
 	require_once("class_zabbix.php");
 	require_once("cookies.php");
-	
+
 	$zabbix = new Zabbix($arrSettings);
-	
+
 	// Populate our class
 	$zabbix->setUsername($zabbixUser);
 	$zabbix->setPassword($zabbixPass);
 	$zabbix->setZabbixApiUrl($zabbixApi);
-	
+
 	// Login
-	if (isset($zabbixAuthHash) && strlen($zabbixAuthHash) > 0) { 
+	if (isset($zabbixAuthHash) && strlen($zabbixAuthHash) > 0) {
 		// Try it with the authentication hash we have
 		$zabbix->setAuthToken($zabbixAuthHash);
 	} elseif (strlen($zabbix->getUsername()) > 0 && strlen($zabbix->getPassword()) > 0 && strlen($zabbix->getZabbixApiUrl()) > 0) {
 		$zabbix->login();
-	} 
+	}
 
 	if (!$zabbix->isLoggedIn()) {
 		header("Location: index.php");
 		exit();
 	}
-	
+
 	$zabbixHostId = (int) $_GET['hostid'];
 	if ($zabbixHostId > 0) {
 		$host 		= $zabbix->getHostById($zabbixHostId);
-		
+
 		// Graphs
 		$graphs 	= $zabbix->getGraphsByHostId($zabbixHostId);
 		$graphs		= $zabbix->sortGraphsByName($graphs);
-		
+
 		// Triggers
 		$triggers	= $zabbix->getTriggersByHostId($zabbixHostId);
 ?>
@@ -40,14 +40,14 @@
 			<h1><?php echo $host->host?></h1>
 			<a class="back" href="#">Back</a>
 		</div>
-        
+
         <h2>Host details</h2>
         <ul class="rounded">
-            <li class="small">Host: <?=$host->host?></li>
-            <li class="small">DNS: <?=$host->dns?></li>
-            <li class="small">IP: <?=$host->ip?></li>            
+            <li class="small">Host: <?php echo $host->host?></li>
+            <li class="small">DNS: <?php echo $host->dns?></li>
+            <li class="small">IP: <?php echo $host->ip?></li>
         </ul>
-		
+
 		<?php
 			if (is_array($graphs) && count($graphs) > 0) {
 		?>
@@ -57,8 +57,8 @@
 			foreach ($graphs as $graph) {
                 ?>
 				<li class="small">
-                    <a href="graph.php?graphid=<?=$graph["graphid"]?>&period=3600">
-                    <img src="images/chart.png" class="icon_list"><?=$graph["name"]?></a>
+                    <a href="graph.php?graphid=<?php echo $graph["graphid"]?>&period=3600">
+                    <img src="images/chart.png" class="icon_list"><?php echo $graph["name"]?></a>
                 </li>
                 <?php
 			}
