dad887acbdfde95339a11e2ee484537e3cc05bf0
Mattias Geniar <m@ttias.be>
2012-07-04 23:03:57 +0200

    Issue #33: SSL version of tracker image

--- template/footer.php
+++ template/footer.php
@@ -1,8 +1,19 @@
 
+<?php
+    /* Only include the tracker on non-SSL connections, since I'm too cheap to
+        actually buy a valid SSL cert
+    */
+    if (array_key_exists('HTTPS', $_SERVER) && strtolower($_SERVER['HTTPS']) == 'on') {
+?>
 <!-- Some basic usage tracking. Nice stats on browser agents etc. 
 So I know on which browser(s) to focus most -->
 <img src="http://www.mozbx.net/images/tracker.png" />
-<?php echo $arrSettings['googleAnalytics']; ?>
+<?php
+    }
+
+    /* Show the Google Analytics URL, only if it's hosted */
+    if ($arrSettings["isHosted"])
+        echo $arrSettings['googleAnalytics'];
+?>
 </body>
 </html>
-
